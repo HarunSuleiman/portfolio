3ce81fab685a98b11d07061e4f3a9f0b24ba1eb2
HarunSuleiman <harunsuleiman55@gmail.com>
2026-06-04 14:34:04 +0300

    deploying

--- portfolio/portfolio/vite.config.js
+++ portfolio/portfolio/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // to deploy on github pages, add the base property with the name of the repository
+  base: "/portfolio",
+});
